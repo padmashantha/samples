26a54ecc9363604199d63505c97f69b94fe0a61c
Pad Rajapakshe <padmashantha@gmail.com>
2019-01-07 09:27:45 -0500

    adding model class

--- spring-demo/src/main/java/com/shanwork/springdemo/controller/DisplayController.java
+++ spring-demo/src/main/java/com/shanwork/springdemo/controller/DisplayController.java
@@ -1,15 +1,27 @@
 package com.shanwork.springdemo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shanwork.springdemo.model.PizzaMenu;
 
 @RestController
 public class DisplayController {
 	
-	@RequestMapping ("/overview")
+	@RequestMapping (value="/overview", method=RequestMethod.GET)
 	public String displayOverview () {
 		
 		return "Welcome to Spicy Pizza";
+	}
+
+	@RequestMapping (value="/menu", method=RequestMethod.GET)
+	public PizzaMenu displayMenu () {
+		PizzaMenu pizzaMenu = new PizzaMenu();
+		pizzaMenu.setId("1");
+		pizzaMenu.setName("Indian Item 1");
+		
+		return pizzaMenu;
 	}
 
 }
---
+++ spring-demo/src/main/java/com/shanwork/springdemo/model/PizzaMenu.java
@@ -0,0 +1,23 @@
+package com.shanwork.springdemo.model;
+
+public class PizzaMenu {
+	
+	private String id;
+	private String name;
+	//private Ingredient ingredient;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
