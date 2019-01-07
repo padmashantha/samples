20410a7ea3feed8b9815bb6962de7a685b9dda29
Pad Rajapakshe <padmashantha@gmail.com>
2019-01-07 09:50:54 -0500

    added junit tests

---
+++ spring-demo/src/test/java/com/shanwork/springdemo/DisplayControllerTest.java
@@ -0,0 +1,37 @@
+package com.shanwork.springdemo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class DisplayControllerTest extends SpringDemoApplicationTests {
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testMenu() throws Exception {
+		mockMvc.perform(get("/menu")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json;charset=UTF-8"))
+				.andExpect(jsonPath("$.id").value("1")).andExpect(jsonPath("$.name").value("Indian Item 1"));
+
+	}
+
+
+	
+}
